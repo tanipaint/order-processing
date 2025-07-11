6ab3c5e306719d4c73d44cf09fcb7830e10a362a
Gyosuke <t-gyosuke@coexist.or.jp>
2025-07-11 14:44:27 +0900

    style: normalize tests/conftest.py quotes after Black formatting

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,4 +6,4 @@ import os
 import sys
 
 # プロジェクトルートを sys.path に追加
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
